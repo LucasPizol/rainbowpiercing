e6c476a54f7dd2ac7d21fa44d1f1de7fa8a59091
Lucas Pizol Ferreira <131599316+LucasPizol@users.noreply.github.com>
2023-10-21 23:53:09 -0300

    Redefinindo página

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,8 @@
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 600) {
-    document.querySelector(".header-nav").classList.add("activeHeader");
-    document.querySelector(".header-content").classList.add("activeHeader");
+const menu = document.querySelector(".menu-hamburger");
+const navMenu = document.querySelector("header nav");
 
-    return
-  } 
+AOS.init();
 
-  document.querySelector(".header-nav").classList.remove("activeHeader");
-  document.querySelector(".header-content").classList.remove("activeHeader");
-  document.querySelector(".header-nav nav").classList.remove("activeNav")
-
+menu.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
 });
-
-
-document.querySelector(".expand-btn").addEventListener('click', () => {
-    document.querySelector(".header-nav nav").classList.toggle("activeNav")
-    document.querySelector(".expand-btn").classList.toggle("activeNav")
-
-})
-
-
-document.querySelector(".home-main-grid").addEventListener('click', () => {
-    document.querySelector(".header-nav nav").classList.remove("activeNav")
-    document.querySelector(".expand-btn").classList.remove("activeNav")
-
-})
